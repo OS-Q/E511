ff529b86871cadac2fc5d132ce00c0cc9dd8bb6f
qitas <qitas@qitas.cn>
2021-03-26 17:24:58 +0800

    fix

--- make/project.mk
+++ make/project.mk
@@ -318,7 +318,7 @@ ifdef SECURE_SIGNED_APPS_ECDSA_SCHEME
 	@echo "App built but not signed. Sign app & partition data before flashing, via espsecure.py:"
 	@echo "espsecure.py sign_data --version 1 --keyfile KEYFILE $(APP_BIN)"
 	@echo "espsecure.py sign_data --version 1 --keyfile KEYFILE $(PARTITION_TABLE_BIN)"
-else 
+else
 	@echo "App built but not signed. Sign app & partition data before flashing, via espsecure.py:"
 	@echo "espsecure.py sign_data --version 2 --keyfile KEYFILE $(APP_BIN)"
 endif
@@ -338,7 +338,7 @@ endif
 
 # If we have `version.txt` then prefer that for extracting IDF version
 ifeq ("$(wildcard ${IDF_PATH}/version.txt)","")
-IDF_VER_T := $(shell cd ${IDF_PATH} && git describe --always --tags --dirty)
+IDF_VER_T := $(shell cd ${IDF_PATH} && git describe --always --dirty --match v*.*)
 else
 IDF_VER_T := $(shell cat ${IDF_PATH}/version.txt)
 endif
@@ -770,4 +770,3 @@ endif #CONFIG_SDK_TOOLPREFIX
 
 #####################################################################
 endif #CONFIG_IDF_TARGET
-
